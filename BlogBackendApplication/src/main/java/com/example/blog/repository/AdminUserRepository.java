c33f34f2ee2a011a8c1653cbd0710595da99daf1
Amit Budhodkar <116152541+amit1725@users.noreply.github.com>
2025-08-22 15:13:57 +0530

    Delete BlogBackendApplication directory

--- BlogBackendApplication/src/main/java/com/example/blog/repository/AdminUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.blog.repository;
-
-import com.example.blog.model.AdminUser;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface AdminUserRepository extends MongoRepository<AdminUser, String> {
-    Optional<AdminUser> findByUsername(String username);
-}
